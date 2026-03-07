c16cf5fe299d7c9c3195afcd807570c625a3b8af
Amilkar <ing_amilkar@hotmail.com>
2026-03-07 18:15:53 -0500

    add socket endpoint

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000");
+export const socket = io("https://fluxen.store/banckend", {
+  transports: ["websocket"], // Fuerza WebSocket
+  secure: true, // HTTPS
+});
